16200b4a574647334082500d5dcd83e24df4175d
Anas <anastarek10777@gmail.com>
2026-02-06 16:23:37 +0200

    Fix String type in shared library functions

--- vars/scanDockerImage.groovy
+++ vars/scanDockerImage.groovy
@@ -1,3 +1,3 @@
-def call(string imageName) {
+def call(String imageName) {
     sh "trivy image ${imageName}:${env.BUILD_NUMBER}"
 }
--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,10 +1,9 @@
-def call(string sonarTokenCredID) {
+def call(String sonarTokenCredID) {
     withCredentials([string(credentialsId: sonarTokenCredID, variable: 'SONAR_TOKEN')]) {
-        sh """
-            cd jenkins-ci-cd/lab-23/Jenkins_App
-            mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \\
-                -Dsonar.projectKey=Anas990978_Ivolve_Training \\
-                -Dsonar.token=\$SONAR_TOKEN
-        """
+        sh '''
+            mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
+                -Dsonar.projectKey=Anas990978_Ivolve_Training \
+                -Dsonar.token=$SONAR_TOKEN
+        '''
     }
 }
