287663fd3de69c339b2b5438763e2e2a8b7122f6
Anas Tarek <90289874+Anas990978@users.noreply.github.com>
2026-02-27 21:59:44 +0200

    Update app.py

--- docker/lab-8/Docker5/backend/app.py
+++ docker/lab-8/Docker5/backend/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello from Backend!"
-    
-app.run(host='0.0.0.0', port=5000)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
